cffef45bcc52d0d225a9591087adb5a2dcff54e6
Xiang <cxiang@connect.hku.hk>
2026-02-07 23:18:35 +0800

    fix: casing error

--- featureVerification/src/routes/__root.tsx
+++ featureVerification/src/routes/__root.tsx
@@ -12,7 +12,7 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
-import Header from '@/components/header.tsx'
+import Header from '@/components/Header'
 
 interface MyRouterContext {
   queryClient: QueryClient
